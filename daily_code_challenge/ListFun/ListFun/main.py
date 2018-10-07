5673de07151be3a526166cb12902a6af8dc3939a
hakkoda <xcodula@icloud.com>
2018-10-07 05:44:03 -0700

    daily code challenge updates

--- daily_code_challenge/ListFun/ListFun/main.py
+++ daily_code_challenge/ListFun/ListFun/main.py
@@ -9,8 +9,27 @@ def find_sum(num_list):
 
     return result
 
+def copy_list(num_list):
+    result = []
+
+    for x in num_list:
+        result.append(x)
+
+    return result
+
 def sort_asc(num_list):
-    return None
+    result = copy_list(num_list)
+
+    for target_index in range(0, len(result)):
+        target = result[target_index]
+        for result_index in range(target_index, len(result)):
+            if result[result_index] > target:
+                temp = result[result_index]
+                result[result_index] = target
+                result[target_index] = temp
+                target = temp
+
+    return result
 
 def find_largest(num_list):
     result = num_list[0]
--- daily_code_challenge/ListFun/tests/ListFun_tests.py
+++ daily_code_challenge/ListFun/tests/ListFun_tests.py
@@ -15,3 +15,8 @@ def test_sort_asc():
     target = [ 1, 2, 3 ]
     target_sorted = sort_asc(target)
     assert_equal([ 3, 2, 1 ], target_sorted)
+
+def test_sort_asc():
+    target = [ 2, 9, 9, 3, 8, 5, 4, 0, 1 ]
+    target_sorted = sort_asc(target)
+    assert_equal([ 9, 9, 8, 5, 4, 3, 2, 1, 0 ], target_sorted)
