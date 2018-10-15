5adff0922b5a31ab064ad396f04a03ee506ccdee
hakkoda <xcodula@icloud.com>
2018-10-15 05:12:39 -0700

    Added some animation while moving a sprite

--- cocos2d/SpriteDemo01/SpriteDemo01/main.py
+++ cocos2d/SpriteDemo01/SpriteDemo01/main.py
@@ -12,9 +12,32 @@ class SpriteDemo01(cocos.layer.ColorLayer):
         sprite1.do(Repeat(RotateBy(360, duration=2)))
         self.add(sprite1)
 
-        self.sprite2 = cocos.sprite.Sprite("img/invaders.png")
+        # static image sprite
+        #self.sprite2 = cocos.sprite.Sprite("img/invaders.png")
+        #self.sprite2.position = 32,24 
+        #self.sprite2.scale = 2
+        #self.sprite2.speed = 150
+        #self.sprite2.velocity = 0, 0
+        #self.add(self.sprite2)
+
+        # trying out animated sprite
+        pyglet.resource.path = ["img"]
+        pyglet.resource.reindex()
+
+        # animate with gif -- useful, but I don't feel like I have good control
+        # on the timing of the animation.
+        #invader = pyglet.resource.animation("invaders.gif")
+        #self.sprite2 = cocos.sprite.Sprite(invader)
+
+        # animate with sprite sheet
+        invader = pyglet.resource.image("animated_invaders.png")
+        self.invader_seq = pyglet.image.ImageGrid(invader, 1, 2)
+        self.animated_img = pyglet.image.Animation.from_image_sequence(self.invader_seq[0:], 0.1, loop=True)
+        #self.sprite2 = cocos.sprite.Sprite(self.animated_img)  # during movement
+        self.sprite2 = cocos.sprite.Sprite(self.invader_seq[0]) # during rest
+
         self.sprite2.position = 32,24 
-        self.sprite2.scale = 2
+        self.sprite2.scale = 1
         self.sprite2.speed = 150
         self.sprite2.velocity = 0, 0
         self.add(self.sprite2)
@@ -51,19 +74,23 @@ class SpriteDemo01(cocos.layer.ColorLayer):
                 self.x_pressed = True
                 self.x_released = False
                 self.sprite2.velocity = value * self.sprite2.speed, 0
+                self.sprite2.image = self.animated_img
             elif self.x_pressed:
                 self.x_released = True
                 self.x_pressed = False
                 self.sprite2.velocity = 0,0
+                self.sprite2.image = self.invader_seq[0]
         elif axis == "y":
             if value == 1.0 or value == -1.0:
                 self.y_pressed = True
                 self.y_released = False
                 self.sprite2.velocity = 0, -value * self.sprite2.speed
+                self.sprite2.image = self.animated_img
             elif self.y_pressed:
                 self.y_released = True
                 self.y_pressed = False
                 self.sprite2.velocity = 0,0
+                self.sprite2.image = self.invader_seq[0]
 
 if __name__ == "__main__":
     cocos.director.director.init()
