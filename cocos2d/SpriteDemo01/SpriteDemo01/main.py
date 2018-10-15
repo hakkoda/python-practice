7d41469b587aef9326235df341ebfc2f9478d1c4
hakkoda <xcodula@icloud.com>
2018-10-15 06:06:17 -0700

    Created a PlayerSprite to abstract away sprite movement/animation

--- cocos2d/SpriteDemo01/SpriteDemo01/main.py
+++ cocos2d/SpriteDemo01/SpriteDemo01/main.py
@@ -2,6 +2,36 @@ import pyglet
 import cocos
 from cocos.actions import *
 
+class PlayerSprite(cocos.sprite.Sprite):
+    def __init__(self):
+        pyglet.resource.path = ["img"]
+        pyglet.resource.reindex()
+        invader = pyglet.resource.image("animated_invaders.png")
+        self.invader_seq = pyglet.image.ImageGrid(invader, 1, 2)
+        self.animated_img = pyglet.image.Animation.from_image_sequence(self.invader_seq[0:], 0.1, loop=True)
+        super(PlayerSprite, self).__init__(self.invader_seq[0])
+        #self.sprite2 = cocos.sprite.Sprite(self.animated_img)  # during movement
+        #self.sprite2 = cocos.sprite.Sprite(self.invader_seq[0]) # during rest
+        self.position = 32,24 
+        self.scale = 1
+        self.speed = 150
+        self.velocity = 0, 0
+
+    def rest(self):
+        self.velocity = 0,0
+        self.image = self.invader_seq[0]
+
+    def move_x(self, value):
+        current_y_velocity = self.velocity[1]
+        self.velocity = value * self.speed, current_y_velocity
+        self.image = self.animated_img
+
+    def move_y(self, value):
+        current_x_velocity = self.velocity[0]
+        self.velocity = current_x_velocity, -value * self.speed
+        self.image = self.animated_img
+
+
 class SpriteDemo01(cocos.layer.ColorLayer):
     def __init__(self):
         super(SpriteDemo01, self).__init__(0,0,0,0)
@@ -12,34 +42,16 @@ class SpriteDemo01(cocos.layer.ColorLayer):
         sprite1.do(Repeat(RotateBy(360, duration=2)))
         self.add(sprite1)
 
-        # static image sprite
-        #self.sprite2 = cocos.sprite.Sprite("img/invaders.png")
-        #self.sprite2.position = 32,24 
-        #self.sprite2.scale = 2
-        #self.sprite2.speed = 150
-        #self.sprite2.velocity = 0, 0
-        #self.add(self.sprite2)
+        ## trying out animated sprite
+        #pyglet.resource.path = ["img"]
+        #pyglet.resource.reindex()
 
-        # trying out animated sprite
-        pyglet.resource.path = ["img"]
-        pyglet.resource.reindex()
+        ## animate with gif -- useful, but I don't feel like I have good control
+        ## on the timing of the animation.
+        ##invader = pyglet.resource.animation("invaders.gif")
+        ##self.sprite2 = cocos.sprite.Sprite(invader)
 
-        # animate with gif -- useful, but I don't feel like I have good control
-        # on the timing of the animation.
-        #invader = pyglet.resource.animation("invaders.gif")
-        #self.sprite2 = cocos.sprite.Sprite(invader)
-
-        # animate with sprite sheet
-        invader = pyglet.resource.image("animated_invaders.png")
-        self.invader_seq = pyglet.image.ImageGrid(invader, 1, 2)
-        self.animated_img = pyglet.image.Animation.from_image_sequence(self.invader_seq[0:], 0.1, loop=True)
-        #self.sprite2 = cocos.sprite.Sprite(self.animated_img)  # during movement
-        self.sprite2 = cocos.sprite.Sprite(self.invader_seq[0]) # during rest
-
-        self.sprite2.position = 32,24 
-        self.sprite2.scale = 1
-        self.sprite2.speed = 150
-        self.sprite2.velocity = 0, 0
+        self.sprite2 = PlayerSprite()
         self.add(self.sprite2)
 
         # setup joysticks
@@ -51,12 +63,26 @@ class SpriteDemo01(cocos.layer.ColorLayer):
         self.joystick.on_joyaxis_motion = self.on_joyaxis_motion
         self.joystick.open()
 
-        self.x_pressed = False
-        self.y_pressed = False
-        self.x_released = False
-        self.y_released = False
+        self.x_button_status = "RELEASED"
+        self.y_button_status = "RELEASED"
 
         self.sprite2.do(Move())
+
+    def on_joyaxis_motion(self, joystick, axis, value):
+        if axis == "x":
+            if value == 1.0 or value == -1.0:
+                self.x_button_status = "PRESSED"
+                self.sprite2.move_x(value)
+            elif self.x_button_status == "PRESSED":
+                self.x_button_status = "RELEASED"
+                self.sprite2.rest()
+        elif axis == "y":
+            if value == 1.0 or value == -1.0:
+                self.y_button_status = "PRESSED"
+                self.sprite2.move_y(value)
+            elif self.y_button_status == "PRESSED":
+                self.y_button_status = "RELEASED"
+                self.sprite2.rest()
 
     def on_joybutton_press(self, joystick, button):
         print("button pressed...")
@@ -68,29 +94,6 @@ class SpriteDemo01(cocos.layer.ColorLayer):
         print(f"button: {button}")
         self.sprite2.speed = 150
 
-    def on_joyaxis_motion(self, joystick, axis, value):
-        if axis == "x":
-            if value == 1.0 or value == -1.0:
-                self.x_pressed = True
-                self.x_released = False
-                self.sprite2.velocity = value * self.sprite2.speed, 0
-                self.sprite2.image = self.animated_img
-            elif self.x_pressed:
-                self.x_released = True
-                self.x_pressed = False
-                self.sprite2.velocity = 0,0
-                self.sprite2.image = self.invader_seq[0]
-        elif axis == "y":
-            if value == 1.0 or value == -1.0:
-                self.y_pressed = True
-                self.y_released = False
-                self.sprite2.velocity = 0, -value * self.sprite2.speed
-                self.sprite2.image = self.animated_img
-            elif self.y_pressed:
-                self.y_released = True
-                self.y_pressed = False
-                self.sprite2.velocity = 0,0
-                self.sprite2.image = self.invader_seq[0]
 
 if __name__ == "__main__":
     cocos.director.director.init()
